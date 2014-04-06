c96af712dcc096f682137ff698f55638587c392e
jdnguyen <jdnguyenxo@gmail.com>
2014-04-06 08:29:26 -0700

    calling .to_s on ids

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   def json_for_events(events)
     Jbuilder.encode do |json|
       json.array!(events) do |event|
-        json.id event.id
+        json.id event.id.to_s
         json.location_name event.location_name
         json.start_time event.start_time.strftime('%-m/%-d/%y %l:%M%P')
         json.artists_names event.artists_names
@@ -18,7 +18,7 @@ module ApplicationHelper
 
   def json_for_event(event)
     Jbuilder.encode do |json|
-      json.id event.id
+      json.id event.id.to_s
       json.location_name event.location_name
       json.start_time event.start_time.strftime('%-m/%-d/%y %l:%M%P')
       json.artists_names event.artists_names
