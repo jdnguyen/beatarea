8f029c7ea7e3d6ffc7042e677e8f34761d7e197e
jdnguyen <jdnguyenxo@gmail.com>
2014-04-06 21:29:39 -0700

    changing thumbnail dimensions

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,6 +14,6 @@ class Artist
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  has_mongoid_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  has_mongoid_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
