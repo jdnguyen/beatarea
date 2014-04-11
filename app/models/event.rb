fef81b9fc48581445355c981288ed540d68a926a
jdnguyen <jdnguyenxo@gmail.com>
2014-04-10 21:02:42 -0700

    added accepts_nested_attributes_for notifications on events

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,6 +14,7 @@ class Event
 
   has_and_belongs_to_many :artists
   embeds_many :notifications
+  accepts_nested_attributes_for :notifications
 
   has_mongoid_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
